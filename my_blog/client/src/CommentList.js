dac1b653ea9f081cbc243279ea659649ae52f831
LucasLazzaris <lucascercal.l@gmail.com>
2021-12-15 09:27:31 -0300

    Communicate services using a event_bus and add a new moderation service

--- my_blog/client/src/CommentList.js
+++ my_blog/client/src/CommentList.js
@@ -1,23 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-export default function CommentList({ postId }) {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
+export default function CommentList({ comments }) {
   const renderedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    if (comment.status === 'approved') {
+      content = comment.content;
+    } else if (comment.status === 'pending'){
+      content = 'This comment is awaiting moderation';
+    } else {
+      content = 'This comment has been rejected'
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
+
   return <ul>{renderedComments}</ul>;
 }
