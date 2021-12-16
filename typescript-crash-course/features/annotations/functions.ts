45868c8cb8d920601662de977e086c1eeaad684c
LucasLazzaris <lucascercal.l@gmail.com>
2021-12-16 18:53:01 -0300

    Crash course on ts annotations

---
+++ typescript-crash-course/features/annotations/functions.ts
@@ -0,0 +1,46 @@
+const add = (a: number, b: number): number => {
+  return a + b;
+};
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+const multiply = function (a: number, b: number): number {
+  return a * b;
+};
+
+const logger = (message: string): void => {
+  console.log(message);
+};
+
+// Always error
+const throwError = (message: string): never => {
+  throw new Error(message);
+};
+
+// Sometimes error
+const throwErrorOrMessage = (message: string): string => {
+  if (!message) {
+    throw new Error(message);
+  }
+  return message;
+};
+
+const todaysWeather = {
+  date: new Date(),
+  weather: 'sunny',
+};
+
+const logWeather = ({
+  date,
+  weather,
+}: {
+  date: Date;
+  weather: string;
+}): void => {
+  console.log(date);
+  console.log(weather);
+};
+
+logWeather(todaysWeather);
---
+++ typescript-crash-course/features/types.ts
@@ -0,0 +1,15 @@
+const today = new Date();
+
+today.getMonth();
+today.garbage; // Compiler error
+
+const person = {
+  age: 20
+};
+
+person.garbage; // Compiler error
+
+class Color {
+
+}
+const red = new Color();
