6c93066667785135a4e95ace0077aad1deb2f483
LucasLazzaris <lucascercal.l@gmail.com>
2021-12-16 22:09:04 -0300

    Add small map app

---
+++ typescript-crash-course/maps/src/CustomMap.ts
@@ -0,0 +1,35 @@
+interface Mappable {
+  location: {
+    lat: number;
+    lng: number;
+  };
+  markerContent(): string;
+}
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    this.googleMap = new google.maps.Map(document.getElementById(divId), {
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+      zoom: 1,
+    });
+  }
+
+  addMarker(mappable: Mappable): void {
+    const marker = new google.maps.Marker({
+      map: this.googleMap,
+      position: mappable.location,
+    });
+
+    marker.addListener('click', () => {
+      const infoWindow = new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
+      infoWindow.open(this.googleMap, marker);
+    });
+  }
+}
