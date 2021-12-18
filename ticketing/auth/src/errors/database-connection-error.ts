bbcef4b2037868d351a7a108432584a6a6451e6f
LucasLazzaris <lucascercal.l@gmail.com>
2021-12-18 09:09:04 -0300

    Create infra for auth service

---
+++ ticketing/auth/src/errors/database-connection-error.ts
@@ -0,0 +1,20 @@
+import { CustomError } from "./custom-error";
+
+export class DataBaseConnectionError extends CustomError {
+  statusCode = 500;
+  reason = 'Error connecting to database';
+
+  constructor() {
+    super('Error with parameters');
+
+    Object.setPrototypeOf(this, DataBaseConnectionError.prototype);
+  }
+
+  serializeErrors() {
+    return [
+      {
+        message: this.reason,
+      },
+    ];
+  }
+}
