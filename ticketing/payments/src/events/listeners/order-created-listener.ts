ad42f6ec8103a2bf9e4112474df839c7cd095600
LucasLazzaris <lucascercal.l@gmail.com>
2022-01-16 12:50:31 -0300

    Create payments services

---
+++ ticketing/payments/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,23 @@
+import { Listener, OrderCreatedEvent, Subjects } from '@lazzaris/common';
+import { Message } from 'node-nats-streaming';
+import { Order } from '../../models/order';
+import { queueGroupName } from './queue-group-name';
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  subject: Subjects.OrderCreated = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const order = Order.build({
+      id: data.id,
+      price: data.ticket.price,
+      status: data.orderStatus,
+      version: data.version,
+      userId: data.userId,
+    });
+
+    await order.save()
+
+    msg.ack();
+  }
+}
